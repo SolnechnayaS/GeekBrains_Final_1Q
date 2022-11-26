9642b8ce3f4154df466455fc22b2fa7e88ad5239
Svetlana Morozova <svetlanochka@yandex.ru>
2022-11-26 23:07:35 +0300

    Написан код для решения итоговой задачи простым способом

---
+++ Final_SimplePrint/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Clear();
+Console.Write("Сколько строк с данными планируешь ввести? ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+
+string[] array = new string[n];
+
+void FillArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Convert.ToString(Console.ReadLine());
+    }
+}
+
+Console.WriteLine("Введи строки массива, разделяя их enter: ");
+FillArray(array);
+
+void TrinityArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            Console.WriteLine(array[i]);
+        }
+    }
+}
+
+Console.WriteLine("Строки, длина которых меньше либо равна 3 символa:");
+TrinityArray(array);
